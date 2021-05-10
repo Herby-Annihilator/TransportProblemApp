798290feda5c565a0e8c7b0bb9eece5132be388e
Rukin Danil <rukin2018d@yandex.ru>
2021-05-10 15:03:54 +0700

    Закончил с интерфейсом

--- TransportProblemForm.Designer.cs
+++ TransportProblemForm.Designer.cs
@@ -30,32 +30,36 @@ namespace TransportProblemApp
 		private void InitializeComponent()
 		{
 			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.groupBox5 = new System.Windows.Forms.GroupBox();
+			this.optimalSolution = new System.Windows.Forms.DataGridView();
 			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
 			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.tariffMatrix = new System.Windows.Forms.DataGridView();
 			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.stocks = new System.Windows.Forms.DataGridView();
 			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.needs = new System.Windows.Forms.DataGridView();
 			this.panel1 = new System.Windows.Forms.Panel();
 			this.buttonSolveProblem = new System.Windows.Forms.Button();
-			this.tariffMatrix = new System.Windows.Forms.DataGridView();
-			this.stocks = new System.Windows.Forms.DataGridView();
-			this.needs = new System.Windows.Forms.DataGridView();
 			this.groupBox4 = new System.Windows.Forms.GroupBox();
-			this.groupBox5 = new System.Windows.Forms.GroupBox();
 			this.referenceSolution = new System.Windows.Forms.DataGridView();
-			this.optimalSolution = new System.Windows.Forms.DataGridView();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.labelBasePrice = new System.Windows.Forms.Label();
+			this.labelOptimalPrice = new System.Windows.Forms.Label();
 			this.tableLayoutPanel1.SuspendLayout();
+			this.groupBox5.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.optimalSolution)).BeginInit();
 			this.tableLayoutPanel2.SuspendLayout();
 			this.groupBox1.SuspendLayout();
-			this.groupBox2.SuspendLayout();
-			this.groupBox3.SuspendLayout();
-			this.panel1.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.tariffMatrix)).BeginInit();
+			this.groupBox2.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.stocks)).BeginInit();
+			this.groupBox3.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.needs)).BeginInit();
+			this.panel1.SuspendLayout();
 			this.groupBox4.SuspendLayout();
-			this.groupBox5.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.referenceSolution)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.optimalSolution)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// tableLayoutPanel1
@@ -72,8 +76,33 @@ namespace TransportProblemApp
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-			this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(974, 466);
 			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// groupBox5
+			// 
+			this.groupBox5.Controls.Add(this.labelOptimalPrice);
+			this.groupBox5.Controls.Add(this.label2);
+			this.groupBox5.Controls.Add(this.optimalSolution);
+			this.groupBox5.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.groupBox5.Location = new System.Drawing.Point(3, 352);
+			this.groupBox5.Name = "groupBox5";
+			this.groupBox5.Size = new System.Drawing.Size(968, 111);
+			this.groupBox5.TabIndex = 2;
+			this.groupBox5.TabStop = false;
+			this.groupBox5.Text = "Оптимальное решение";
+			// 
+			// optimalSolution
+			// 
+			this.optimalSolution.AllowUserToAddRows = false;
+			this.optimalSolution.AllowUserToDeleteRows = false;
+			this.optimalSolution.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.optimalSolution.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.optimalSolution.Location = new System.Drawing.Point(3, 19);
+			this.optimalSolution.Name = "optimalSolution";
+			this.optimalSolution.RowTemplate.Height = 25;
+			this.optimalSolution.Size = new System.Drawing.Size(607, 85);
+			this.optimalSolution.TabIndex = 2;
 			// 
 			// tableLayoutPanel2
 			// 
@@ -90,7 +119,7 @@ namespace TransportProblemApp
 			this.tableLayoutPanel2.RowCount = 2;
 			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
 			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
-			this.tableLayoutPanel2.Size = new System.Drawing.Size(794, 219);
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(968, 227);
 			this.tableLayoutPanel2.TabIndex = 0;
 			// 
 			// groupBox1
@@ -99,49 +128,10 @@ namespace TransportProblemApp
 			this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.groupBox1.Location = new System.Drawing.Point(3, 3);
 			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Size = new System.Drawing.Size(607, 147);
+			this.groupBox1.Size = new System.Drawing.Size(741, 152);
 			this.groupBox1.TabIndex = 0;
 			this.groupBox1.TabStop = false;
 			this.groupBox1.Text = "Матрица тарифов";
-			// 
-			// groupBox2
-			// 
-			this.groupBox2.Controls.Add(this.stocks);
-			this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.groupBox2.Location = new System.Drawing.Point(616, 3);
-			this.groupBox2.Name = "groupBox2";
-			this.groupBox2.Size = new System.Drawing.Size(175, 147);
-			this.groupBox2.TabIndex = 1;
-			this.groupBox2.TabStop = false;
-			this.groupBox2.Text = "Наличие";
-			// 
-			// groupBox3
-			// 
-			this.groupBox3.Controls.Add(this.needs);
-			this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.groupBox3.Location = new System.Drawing.Point(3, 156);
-			this.groupBox3.Name = "groupBox3";
-			this.groupBox3.Size = new System.Drawing.Size(607, 60);
-			this.groupBox3.TabIndex = 2;
-			this.groupBox3.TabStop = false;
-			this.groupBox3.Text = "Потребности";
-			// 
-			// panel1
-			// 
-			this.panel1.Controls.Add(this.buttonSolveProblem);
-			this.panel1.Location = new System.Drawing.Point(616, 156);
-			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(175, 60);
-			this.panel1.TabIndex = 3;
-			// 
-			// buttonSolveProblem
-			// 
-			this.buttonSolveProblem.Location = new System.Drawing.Point(67, 37);
-			this.buttonSolveProblem.Name = "buttonSolveProblem";
-			this.buttonSolveProblem.Size = new System.Drawing.Size(102, 23);
-			this.buttonSolveProblem.TabIndex = 0;
-			this.buttonSolveProblem.Text = "Решить задачу";
-			this.buttonSolveProblem.UseVisualStyleBackColor = true;
 			// 
 			// tariffMatrix
 			// 
@@ -153,8 +143,19 @@ namespace TransportProblemApp
 			this.tariffMatrix.Location = new System.Drawing.Point(3, 19);
 			this.tariffMatrix.Name = "tariffMatrix";
 			this.tariffMatrix.RowTemplate.Height = 25;
-			this.tariffMatrix.Size = new System.Drawing.Size(601, 125);
+			this.tariffMatrix.Size = new System.Drawing.Size(735, 130);
 			this.tariffMatrix.TabIndex = 0;
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.stocks);
+			this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.groupBox2.Location = new System.Drawing.Point(750, 3);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(215, 152);
+			this.groupBox2.TabIndex = 1;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Наличие";
 			// 
 			// stocks
 			// 
@@ -166,8 +167,19 @@ namespace TransportProblemApp
 			this.stocks.Location = new System.Drawing.Point(3, 19);
 			this.stocks.Name = "stocks";
 			this.stocks.RowTemplate.Height = 25;
-			this.stocks.Size = new System.Drawing.Size(169, 125);
+			this.stocks.Size = new System.Drawing.Size(209, 130);
 			this.stocks.TabIndex = 1;
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.Controls.Add(this.needs);
+			this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.groupBox3.Location = new System.Drawing.Point(3, 161);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Size = new System.Drawing.Size(741, 63);
+			this.groupBox3.TabIndex = 2;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "Потребности";
 			// 
 			// needs
 			// 
@@ -179,30 +191,39 @@ namespace TransportProblemApp
 			this.needs.Location = new System.Drawing.Point(3, 19);
 			this.needs.Name = "needs";
 			this.needs.RowTemplate.Height = 25;
-			this.needs.Size = new System.Drawing.Size(601, 38);
+			this.needs.Size = new System.Drawing.Size(735, 41);
 			this.needs.TabIndex = 2;
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.buttonSolveProblem);
+			this.panel1.Location = new System.Drawing.Point(750, 161);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(209, 60);
+			this.panel1.TabIndex = 3;
+			// 
+			// buttonSolveProblem
+			// 
+			this.buttonSolveProblem.Location = new System.Drawing.Point(65, 19);
+			this.buttonSolveProblem.Name = "buttonSolveProblem";
+			this.buttonSolveProblem.Size = new System.Drawing.Size(102, 23);
+			this.buttonSolveProblem.TabIndex = 0;
+			this.buttonSolveProblem.Text = "Решить задачу";
+			this.buttonSolveProblem.UseVisualStyleBackColor = true;
+			this.buttonSolveProblem.Click += new System.EventHandler(this.buttonSolveProblem_Click);
 			// 
 			// groupBox4
 			// 
+			this.groupBox4.Controls.Add(this.labelBasePrice);
+			this.groupBox4.Controls.Add(this.label1);
 			this.groupBox4.Controls.Add(this.referenceSolution);
 			this.groupBox4.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.groupBox4.Location = new System.Drawing.Point(3, 228);
+			this.groupBox4.Location = new System.Drawing.Point(3, 236);
 			this.groupBox4.Name = "groupBox4";
-			this.groupBox4.Size = new System.Drawing.Size(794, 106);
+			this.groupBox4.Size = new System.Drawing.Size(968, 110);
 			this.groupBox4.TabIndex = 1;
 			this.groupBox4.TabStop = false;
 			this.groupBox4.Text = "Опорное решение";
-			// 
-			// groupBox5
-			// 
-			this.groupBox5.Controls.Add(this.optimalSolution);
-			this.groupBox5.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.groupBox5.Location = new System.Drawing.Point(3, 340);
-			this.groupBox5.Name = "groupBox5";
-			this.groupBox5.Size = new System.Drawing.Size(794, 107);
-			this.groupBox5.TabIndex = 2;
-			this.groupBox5.TabStop = false;
-			this.groupBox5.Text = "Оптимальное решение";
 			// 
 			// referenceSolution
 			// 
@@ -210,47 +231,69 @@ namespace TransportProblemApp
 			this.referenceSolution.AllowUserToDeleteRows = false;
 			this.referenceSolution.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
 			this.referenceSolution.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.referenceSolution.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.referenceSolution.Location = new System.Drawing.Point(3, 19);
 			this.referenceSolution.Name = "referenceSolution";
 			this.referenceSolution.RowTemplate.Height = 25;
-			this.referenceSolution.Size = new System.Drawing.Size(788, 84);
+			this.referenceSolution.Size = new System.Drawing.Size(607, 84);
 			this.referenceSolution.TabIndex = 1;
 			// 
-			// optimalSolution
+			// label1
 			// 
-			this.optimalSolution.AllowUserToAddRows = false;
-			this.optimalSolution.AllowUserToDeleteRows = false;
-			this.optimalSolution.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-			this.optimalSolution.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.optimalSolution.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.optimalSolution.Location = new System.Drawing.Point(3, 19);
-			this.optimalSolution.Name = "optimalSolution";
-			this.optimalSolution.RowTemplate.Height = 25;
-			this.optimalSolution.Size = new System.Drawing.Size(788, 85);
-			this.optimalSolution.TabIndex = 2;
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(619, 56);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(189, 15);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Стоимость опорного плана (у.е.)";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(619, 55);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(215, 15);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Стоимость оптимального плана (у.е.)";
+			// 
+			// labelBasePrice
+			// 
+			this.labelBasePrice.AutoSize = true;
+			this.labelBasePrice.Location = new System.Drawing.Point(815, 56);
+			this.labelBasePrice.Name = "labelBasePrice";
+			this.labelBasePrice.Size = new System.Drawing.Size(0, 15);
+			this.labelBasePrice.TabIndex = 3;
+			// 
+			// labelOptimalPrice
+			// 
+			this.labelOptimalPrice.AutoSize = true;
+			this.labelOptimalPrice.Location = new System.Drawing.Point(841, 55);
+			this.labelOptimalPrice.Name = "labelOptimalPrice";
+			this.labelOptimalPrice.Size = new System.Drawing.Size(0, 15);
+			this.labelOptimalPrice.TabIndex = 4;
 			// 
 			// TransportProblemForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(974, 466);
 			this.Controls.Add(this.tableLayoutPanel1);
 			this.Name = "TransportProblemForm";
 			this.Text = "TransportProblemForm";
 			this.tableLayoutPanel1.ResumeLayout(false);
+			this.groupBox5.ResumeLayout(false);
+			this.groupBox5.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.optimalSolution)).EndInit();
 			this.tableLayoutPanel2.ResumeLayout(false);
 			this.groupBox1.ResumeLayout(false);
-			this.groupBox2.ResumeLayout(false);
-			this.groupBox3.ResumeLayout(false);
-			this.panel1.ResumeLayout(false);
 			((System.ComponentModel.ISupportInitialize)(this.tariffMatrix)).EndInit();
+			this.groupBox2.ResumeLayout(false);
 			((System.ComponentModel.ISupportInitialize)(this.stocks)).EndInit();
+			this.groupBox3.ResumeLayout(false);
 			((System.ComponentModel.ISupportInitialize)(this.needs)).EndInit();
+			this.panel1.ResumeLayout(false);
 			this.groupBox4.ResumeLayout(false);
-			this.groupBox5.ResumeLayout(false);
+			this.groupBox4.PerformLayout();
 			((System.ComponentModel.ISupportInitialize)(this.referenceSolution)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.optimalSolution)).EndInit();
 			this.ResumeLayout(false);
 
 		}
@@ -271,5 +314,9 @@ namespace TransportProblemApp
 		private System.Windows.Forms.DataGridView optimalSolution;
 		private System.Windows.Forms.GroupBox groupBox4;
 		private System.Windows.Forms.DataGridView referenceSolution;
+		private System.Windows.Forms.Label labelOptimalPrice;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label labelBasePrice;
+		private System.Windows.Forms.Label label1;
 	}
 }
--- TransportProblemForm.cs
+++ TransportProblemForm.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using TransportProblemApp.Model;
 
 namespace TransportProblemApp
 {
@@ -12,6 +13,7 @@ namespace TransportProblemApp
 	{
 		private int _providersCount;
 		private int _consumersCount;
+
 		public TransportProblemForm(int providersCount, int consumersCount)
 		{
 			InitializeComponent();
@@ -47,6 +49,83 @@ namespace TransportProblemApp
 				needs.Columns.Add(new DataGridViewTextBoxColumn());
 			}
 			needs.Rows.Add(1);
+		}
+
+		private void buttonSolveProblem_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				labelOptimalPrice.Text = "";
+				labelBasePrice.Text = "";
+				double[][] tariffMatrixNumbers = new double[_providersCount][];
+				for (int i = 0; i < _providersCount; i++)
+				{
+					tariffMatrixNumbers[i] = new double[_consumersCount];
+					for (int j = 0; j < _consumersCount; j++)
+					{
+						tariffMatrixNumbers[i][j] = Convert.ToDouble(tariffMatrix.Rows[i].Cells[j].Value);
+					}
+				}
+				double[] needsRow = new double[_consumersCount];
+				for (int i = 0; i < _consumersCount; i++)
+				{
+					needsRow[i] = Convert.ToDouble(needs.Rows[0].Cells[i].Value);
+				}
+				double[] stocksRow = new double[_providersCount];
+				for (int i = 0; i < _providersCount; i++)
+				{
+					stocksRow[i] = Convert.ToDouble(stocks.Rows[i].Cells[0].Value);
+				}
+				TransportProblem problem = new TransportProblem(TransportProblem.CreateTransportTable(tariffMatrixNumbers, stocksRow, needsRow));
+				Answer answer = problem.GetSolution();
+				CreateBasePlanTable(answer.BasePlan);
+				CreateOptimalPlanTable(answer.OptimalPlan);
+				labelBasePrice.Text = answer.BasePlanPrice.ToString();
+				labelOptimalPrice.Text = answer.OptimalPlanPrice.ToString();
+			}
+			catch(Exception ee)
+			{
+				labelOptimalPrice.Text = $"Ошибка: {ee.Message}";
+				labelBasePrice.Text = $"Ошибка: {ee.Message}";
+			}
+		}
+
+		private void CreateBasePlanTable(double[][] basePlan)
+		{
+			int rowsCount = basePlan.GetLength(0);
+			int colsCount = basePlan[0].Length;
+			referenceSolution ??= new DataGridView();
+			for (int i = 0; i < colsCount; i++)
+			{
+				referenceSolution.Columns.Add(new DataGridViewTextBoxColumn());
+			}
+			referenceSolution.Rows.Add(rowsCount);
+			for (int i = 0; i < rowsCount; i++)
+			{
+				for (int j = 0; j < colsCount; j++)
+				{
+					referenceSolution.Rows[i].Cells[j].Value = basePlan[i][j];
+				}
+			}
+		}
+
+		private void CreateOptimalPlanTable(double[][] optimalPlan)
+		{
+			int rowsCount = optimalPlan.GetLength(0);
+			int colsCount = optimalPlan[0].Length;
+			optimalSolution ??= new DataGridView();
+			for (int i = 0; i < colsCount; i++)
+			{
+				optimalSolution.Columns.Add(new DataGridViewTextBoxColumn());
+			}
+			optimalSolution.Rows.Add(rowsCount);
+			for (int i = 0; i < rowsCount; i++)
+			{
+				for (int j = 0; j < colsCount; j++)
+				{
+					optimalSolution.Rows[i].Cells[j].Value = optimalPlan[i][j];
+				}
+			}
 		}
 	}
 }
